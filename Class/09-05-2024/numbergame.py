e8d8d2eea4211a587eb495da8bf08d146c774108
jaypatel200010 <jaypatel200010@gmail.com>
2024-05-10 11:34:03 +0530

    number game

--- Class/09-05-2024/numbergame.py
+++ Class/09-05-2024/numbergame.py
@@ -1,2 +1,13 @@
 import random
 l1 = random.randint(1,50)
+while True:
+    num = int(input("Enter num : "))
+    if num < l1:
+        print("entered num is lesser!!")
+    elif num > l1:
+        print("entered num is greater!!")
+    elif num == l1:
+        print("Congratulation you won!!")
+        break
+    else:
+        print("invalid number")
