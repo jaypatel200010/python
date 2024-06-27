f629f4319d22e19bd3607a9be42ee14d5a90d7cf
jaypatel200010 <jaypatel200010@gmail.com>
2024-06-27 09:08:44 +0530

    first

--- Assignments/Advance python programming/Q12.py
+++ Assignments/Advance python programming/Q12.py
@@ -1,11 +1,11 @@
 # 12] Write a Python program to copy the contents of a file to another file.
-# Open the first file ("module3.3.txt") in read mode
+# Open the first file ("Q11.txt") in read mode
 f1 = open("Q11.txt", "r")
 # Read the contents of the first file
 f1_data = f1.read()
 # Close the first file
 f1.close()
-# Open the second file ("module3.2.txt") in append mode
+# Open the second file ("Q1.txt") in append mode
 f2 = open("Q1.txt", "a")
 # Write the contents of the first file into the second file
 f2.write(f1_data)
